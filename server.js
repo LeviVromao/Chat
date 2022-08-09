4b6bfc94408413204e87c56a89d45a291561748c
Vítor Romão <106583773+LeviVromao@users.noreply.github.com>
2022-08-08 23:57:30 -0300

    initial commit to repository

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let messages = [];
+
+io.on('connection', socket =>{
+    console.log(`Client is connected with id: ${socket.id}`)
+    socket.emit('previousMessage', messages);
+    socket.on('send Message', data=>{
+    messages.push(data);
+
+    socket.broadcast.emit('receivedMessages', data)
+    })
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, "public","index.html"));
+});
+
+server.listen(5173, console.log('Server is running'));
